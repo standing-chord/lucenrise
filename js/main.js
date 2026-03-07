9d1ec822a89c8ae302b6ba17b80c503e0f7c6b1b
standing-chord <sean050929@snu.ac.kr>
2026-03-08 02:01:12 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let isMenuScrolling = false;
+
+
 // smooth scroll
 
 document.querySelectorAll(".menu a").forEach(anchor => {
@@ -10,15 +13,21 @@ const target = document.querySelector(this.getAttribute("href"));
 
 if(target){
 
+isMenuScrolling = true;
+
 target.scrollIntoView({
 behavior:"smooth"
 });
 
-}
-
-/* 메뉴 클릭 시 header 숨기기 */
-
 header.classList.add("header-hide");
+
+/* 스크롤 완료 후 다시 활성화 */
+
+setTimeout(()=>{
+isMenuScrolling = false;
+},800);
+
+}
 
 });
 
